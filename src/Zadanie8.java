afdb81d48f1d027e4f7d0f66b8f89c2c789b5c34
Oliwia171 <oliwiakwasek123@gmail.com>
2025-06-18 12:21:37 +0200

    liczba pierwsza

---
+++ src/Zadanie8.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Zadanie8 {
+
+
+    public static void liczbapierwsza(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.println("Podaj liczbe");
+        int liczba = n.nextInt();
+        boolean pierwsza = true;
+        for (int i = 2; i < Math.sqrt(liczba); i++) {
+            if ((liczba % i == 0)) {
+                pierwsza = false;
+            }
+
+        }
+        if (pierwsza) {
+            System.out.println("Liczba" + liczba + "jest liczba pierwsza");
+        } else {
+            System.out.println("Liczba" + liczba + "nie jest liczba pierwsza");
+        }
+    }
+}
+
+
