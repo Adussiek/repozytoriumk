b0dc34871c82b6d8a44cb2a58b05a51133b821a6
Patrykczudc <p.ziobro@op.pl>
2025-06-18 12:18:55 +0200

    Zadanie4

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,6 @@
-import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
 
-class Main {
-    // Function to find GCD of two numbers using the Euclidean Algorithm
-    public static int findGCD(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
     }
 
-    // Function to find GCD of an array of numbers
-    public static int findGCDofArray(int[] arr) {
-        int result = arr[0]; // Start with the first number
-        for (int i = 1; i < arr.length; i++) {
-            result = findGCD(result, arr[i]);
-            if (result == 1) { // If GCD becomes 1, no need to continue
-                return 1;
-            }
-        }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Taking input for the number of elements
-        System.out.print("Enter the number of elements: ");
-        int n = scanner.nextInt();
-        int[] numbers = new int[n];
-
-        // Taking input for the array elements
-        System.out.println("Enter the numbers:");
-        for (int i = 0; i < n; i++) {
-            numbers[i] = scanner.nextInt();
-        }
-
-        // Finding the GCD of the array
-        int gcd = findGCDofArray(numbers);
-
-        // Displaying the result
-        System.out.println("The GCD of the given numbers is: " + gcd);
-
-        scanner.close();
-    }
 }
